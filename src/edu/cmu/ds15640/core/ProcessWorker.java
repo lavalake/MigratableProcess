f85c22db73d8bceb9e846db887e3386a8b63b3a9
Hao Ge <gehao868@gmail.com>
2014-05-27 18:03:23 -0400

    hge

--- src/edu/cmu/ds15640/core/ProcessWorker.java
+++ src/edu/cmu/ds15640/core/ProcessWorker.java
@@ -1,9 +1,20 @@
 package edu.cmu.ds15640.core;
 
 import java.lang.reflect.Constructor;
+import java.net.Socket;
 
 public class ProcessWorker {
+	private String host;
+	private int port;
 
+	private Socket socket;
+
+
+	public ProcessWorker(String host, int port){
+
+	}
+
+	
 	public static void main(String[] args) {
 		/*
 		processClass = ProcessManager.class.getClassLoader().loadClass(processName);
