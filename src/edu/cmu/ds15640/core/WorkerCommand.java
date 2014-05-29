9ea1108952c4283f309297149bfd6ea7a85653ed
Hao Ge <gehao868@gmail.com>
2014-05-28 22:14:31 -0400

    hge

--- src/edu/cmu/ds15640/core/WorkerCommand.java
+++ src/edu/cmu/ds15640/core/WorkerCommand.java
@@ -4,6 +4,10 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class WorkerCommand implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2215252029261060551L;
 	private CommandType type;
 	private MigratableProcess migratableProcess;
 	//The targetWorkerID is for the target worker the process migrate to
